d43492e44c74a217f8836c0a2283b16c2c3b744d
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-02-07 12:54:09 -0600

    Added ship selector

---
+++ Pocket Rocket/Assets/GetMainShip.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetMainShip : MonoBehaviour
+{
+    public GameObject greenShip, wierdShip, ufoShip;
+    private MeshRenderer myMesh;
+    private readonly string selectedShip = "SelectedShip";
+    private void Awake()
+    {
+        myMesh = this.GetComponent<MeshRenderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        int getShip;
+
+        getShip = PlayerPrefs.GetInt(selectedShip);
+
+        switch (getShip)
+        {
+            case 1:
+                GameObject wierd = wierdShip;
+                break;
+            case 2:
+                GameObject ufo = ufoShip;
+                break;
+            case 3:
+                GameObject green = greenShip;
+                break;
+        }
+    }
+
+}
---
+++ Pocket Rocket/Assets/Scripts/SelectorScript.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using UnityEngine;
+
+public class SelectorScript : MonoBehaviour
+{
+    public GameObject greenShip;
+    public GameObject wierdShip;
+    public GameObject ufoShip;
+
+    Rigidbody greenShipRigibody;
+
+    private Vector3 shipPos;
+    private Vector3 offScreenPos;
+    private int CharacterInt = 1;
+    private MeshRenderer greenShipRenderer, wierdShipRenderer, ufoShipRenderer;
+    private readonly string selectedShip = "SelectedShip";
+
+    private void Awake()
+    {
+        shipPos = new Vector3(-11,3,-1);
+        offScreenPos = new Vector3(-141,0,0);
+        greenShipRenderer = GetComponent<MeshRenderer>();
+        wierdShipRenderer = GetComponent<MeshRenderer>();
+        ufoShipRenderer = GetComponent<MeshRenderer>();
+
+    }
+
+    public void NextCharacter()
+    {
+        switch (CharacterInt)
+        {
+            case 1:
+                PlayerPrefs.SetInt(selectedShip, 1);
+                greenShip.transform.position = offScreenPos;
+                wierdShip.transform.position = shipPos;
+                CharacterInt++;
+                break;
+            case 2:
+                PlayerPrefs.SetInt(selectedShip, 2);
+                wierdShip.transform.position = offScreenPos;
+                ufoShip.transform.position = shipPos;
+                CharacterInt++;
+                
+                break;
+            case 3:
+                PlayerPrefs.SetInt(selectedShip, 3);
+                ufoShip.transform.position = offScreenPos;
+                greenShip.transform.position = shipPos;
+                CharacterInt++;
+                break;
+            default:
+                ResetInt();
+                break;
+        }
+    }
+
+    public void PreviousCharacter()
+    {
+        switch (CharacterInt)
+        {
+            case 1:
+                PlayerPrefs.SetInt(selectedShip,2);
+                greenShip.transform.position = offScreenPos;
+                ufoShip.transform.position = shipPos;
+                CharacterInt++;
+               
+                break;
+            case 2:
+                PlayerPrefs.SetInt(selectedShip, 1);
+                ufoShip.transform.position = offScreenPos;
+                wierdShip.transform.position = shipPos;
+                CharacterInt++;
+                break;
+            case 3:
+                PlayerPrefs.SetInt(selectedShip, 3);
+                wierdShip.transform.position = offScreenPos;
+                greenShip.transform.position = shipPos;
+                CharacterInt++;
+                break;
+            default:
+                ResetInt();
+                break;
+        }
+    }
+
+    private void ResetInt()
+    {
+        if (CharacterInt >= 3)
+        {
+            CharacterInt = 1;
+        }
+        else
+        {
+            CharacterInt = 3;
+        }
+    }
+}
